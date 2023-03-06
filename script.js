70d882ea899210608efadd3a6417be4e8c0e533f
Heavenlydawn <gabrielheavenlydawn@gmail.com>
2023-03-06 16:48:50 +0100

    Landing page modification

---
+++ script.js
@@ -0,0 +1,13 @@
+const shadesOfBlue = [
+  "hsl(205, 72%, 37%)",
+  "hsl(205, 63%, 48%)",
+  "#49a6e9",
+  "hsl(205, 89%, 70%)",
+  "hsl(205, 90%, 76%)",
+  "hsl(205, 86%, 81%)",
+  " hsl(205, 90%, 88%)",
+  "hsl(205, 100%, 96%)",
+];
+
+
+document.querySelector(".right-section")
